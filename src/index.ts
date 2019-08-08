a30e56a08df05c493891df81fae9159d121834b0
Imed Adel <aadelimed@gmail.com>
2019-08-08 18:20:42 +0200

    :construction: install prompts types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import prompts from "prompts"
+
+(async () => {
+  const response = await prompts({
+    type: 'number',
+    name: 'value',
+    message: 'How old are you?',
+    validate: (value:number) => value < 18 ? `Nightclub is 18+ only` : true
+  });
+ 
+  console.log(response); // => { value: 24 }
+})();
+
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
